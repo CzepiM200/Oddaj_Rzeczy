4a625ec753356cf3e7482bc0e380ed58696fd028
Marcin Czepiela <czepimm200@gmail.com>
2020-09-01 13:27:26 +0200

    Added initial componenet and basic scss styles

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import "./_mainPage.scss";
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <section className="main-page">
+      <article>xD</article>
+    </section>
+  );
+};
+
+export default MainPage;
