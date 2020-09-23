cee45ba8449c6e90ebea9d091605cc5d2059cd50
Marcin Czepiela <czepimm200@gmail.com>
2020-09-24 00:29:20 +0200

    Added email validation in Contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,18 @@
 import "./_contact.scss";
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as DecorationIcon } from "../../images/svg/Decoration.svg";
 
 const Contact = () => {
+  const [emailValidation, setEmailValidation] = useState(true);
+  const validateName = () => {};
+
+  const validateEmail = (email) => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    setEmailValidation(regex.test(String(email).toLowerCase()));
+  };
+
+  const validateMessage = () => {};
+
   return (
     <section className="contact">
       <article className="contact__form">
@@ -16,7 +26,13 @@ const Contact = () => {
             </div>
             <div className="contact__form_email">
               <label>Wpisz swój email</label>
-              <input type="email"></input>
+              <input
+                className={!emailValidation ? "error" : null}
+                onChange={(e) => {
+                  validateEmail(e.target.value);
+                }}
+                type="email"
+              ></input>
             </div>
           </div>
           <div className="contact__form_bottom">
