cd4300e992b460422bb00f0037e169028b326b14
CzepiM200 <czepimm200@gmail.com>
2020-10-04 17:38:33 +0200

    Added Logout function

--- src/firebase/authentication.js
+++ src/firebase/authentication.js
@@ -10,11 +10,21 @@ export const SignUpWithEmailAndPassword = (email, password, history) => {
   auth
     .createUserWithEmailAndPassword(email, password)
     .then((cred) => {
-      console.log(cred);
       history.push("/login");
       alert("Konto zostało założone prawidłowo");
     })
     .catch((error) => {
       alert(error.message);
+    });
+};
+
+export const Logout = () => {
+  auth
+    .signOut()
+    .then((cred) => {
+      alert("Wylogowano pomyślnie");
+    })
+    .catch((error) => {
+      alert("W trakcie wylogowywanie wystąpił błąd");
     });
 };
